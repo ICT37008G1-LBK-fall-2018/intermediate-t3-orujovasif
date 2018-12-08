b9cb9bdbb6954804a55c4a9598a4af42a87d96cd
nika <gabodzenika@gmail.com>
2018-12-08 11:41:08 +0400

    shualeduri

---
+++ A02/script.js
@@ -0,0 +1,33 @@
+
+let btn = document.querySelector('#highlight-product'),
+modal = document.querySelector('#modal');
+
+
+btn.addEventListener('click' , getModelById);
+
+function getModelById(){
+
+let fontSize = modal.firstElementChild.value,
+    MODEL = modal.lastElementChild.value,
+    productByModel = document.querySelector(`.phone-list-item[data-brand='${MODEL}']`);
+    modal.classList.toggle('hide');
+
+if((fontSize) && productByModel){
+    modal.classList.toggle('hide');
+    productByModel.style.fontSize = (fontSize + "px");
+    productByModel.style.fontWeight = "bold";
+}else{
+    if(fontSize && MODEL){
+        fontSize = '';
+        MODEL = '';
+        alert('ჩანაწერი ვერ მოიძებნა');
+      
+    }
+    
+}
+
+
+
+
+
+}
